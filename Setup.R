51fde375bb2726c3fbed065e3d9a59bae0df9b32
Mohammed Ali <mali@uplandsoftware.com>
2019-01-01 10:48:54 +0200

    #4 fix logical error

--- Setup.R
+++ Setup.R
@@ -3,7 +3,7 @@ library(tidyverse)
 get_xml_db_rows("D:\\DS Projects\\Drug_Interaction\\drugbank.xml")
 drugbank <- parse_drug_all()
 drug_df_writer <- function(x, name){
-  write_csv(x, file.path(getwd(), paste0(name, "csv")))
+  write_csv(x, file.path(getwd(), paste0(name, ".csv")))
 }
 
 for(i in 1:length(drugbank)){
