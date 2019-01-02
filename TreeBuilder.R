e2a0c4dbafd29b8414543399a3262f7cf2b5219b
Mohammed Ali <mali@uplandsoftware.com>
2019-01-02 11:20:54 +0200

    #5 add drugs tree main idea

--- TreeBuilder.R
+++ TreeBuilder.R
@@ -1,5 +1,25 @@
 library(tidyverse)
-drugs_names <- read_csv(file = "data/drugs.csv")
+library(lubridate)
+drugs <- read_csv(file = "data/drugs.csv")
+drugs$create_year <- year(drugs$created) 
+drugs$create_month <- month(drugs$created)
 drugs_names <- drugs$name
 write(drugs_names, "data//drugs_names.csv")
-drugs <- read_csv("data//drugs_names.csv", col_names = "name")
+drugs_names <- read_csv("data//drugs_names.csv", col_names = "name")
+
+
+# Drug
+
+## Bio Tech
+### Year
+#### Month
+##### gas 
+##### liquid 
+##### solid
+
+## Small Molecule
+### Year
+#### Month
+##### gas 
+##### liquid 
+##### solid
