11d638e4c91dadd02557f9d64611153e843a2cd0
Mohammed Ali <mali@uplandsoftware.com>
2019-01-02 12:14:03 +0200

    #5 add two levels to the tree

--- TreeBuilder.R
+++ TreeBuilder.R
@@ -1,3 +1,4 @@
+library(data.tree)
 library(tidyverse)
 library(lubridate)
 drugs <- read_csv(file = "data/drugs.csv")
@@ -6,7 +7,6 @@ drugs$create_month <- month(drugs$created)
 drugs_names <- drugs$name
 write(drugs_names, "data//drugs_names.csv")
 drugs_names <- read_csv("data//drugs_names.csv", col_names = "name")
-
 
 # Drug
 
@@ -23,3 +23,20 @@ drugs_names <- read_csv("data//drugs_names.csv", col_names = "name")
 ##### gas 
 ##### liquid 
 ##### solid
+drug <- Node$new("Drugs")
+bio_tech <- drug$AddChild("Bio Tech")
+small_molecule <- drug$AddChild("Small Molecule")
+biotech_years <- drugs %>% filter(type == "biotech") %>% select(create_year) %>% distinct()
+for (year in biotech_years$create_year) {
+  bio_tech$AddChild(year)
+}
+
+small_molecule_years <- drugs %>% filter(type == "small molecule") %>% select(create_year) %>% distinct()
+for (year in small_molecule_years$create_year) {
+  small_molecule$AddChild(year)
+}
+small_molecule$children[[1]]$name
+print(drug)
+plot(drug)
+
+## add months level and other levels
